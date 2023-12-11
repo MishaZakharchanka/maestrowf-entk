d215660d5f26db4e8558340f1216c046559060b9
MishaZakharchanka <zakharchanka1@llnl.gov>
2023-12-11 14:13:37 -0800

    Deleting old/unused file

--- entk_plugin.py
+++
@@ -1,7 +0,0 @@
-from os import rename
-import subprocess
-
-rename('backend_entk.py', '../maestrowf/backend_entk.py')
-rename('maestro_entk.patch', '../maestrowf/maestro_entk.patch')
-
-subprocess.call('git apply maestro_entk.patch', cwd = '../maestrowf', shell = True)
